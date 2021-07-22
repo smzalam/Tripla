8bfbeeadd5a255967000d8e3f2bdce229f686834
syedmza <alam.syedmz@gmail.com>
2021-07-21 23:18:51 -0400

    finished landing page

--- frontend/src/pages/PageStyles.js
+++ frontend/src/pages/PageStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Wrapper = styled.div `
-    position: relative;
+    position: ${props => props.pos};
     height: ${props => props.height};
     width: ${props => props.width};
     background-color: ${props => props.bgcolor};
@@ -16,7 +16,65 @@ export const HeaderText = styled.h1 `
     color: ${props => props.color};
     margin: 0;
     padding-top: ${props => props.pad};
-    z-index: 0;
+`;
+
+export const AnimatedText = styled.h1 `
+    font-family: 'Russo One', sans-serif;
+    font-size: 6rem;
+    color: ${props => props.color};
+    margin: 0;
+    padding-top: ${props => props.pad};
+    animation: fade 10s infinite;
+
+    @keyframes fade {
+        0% {
+            opacity: 0;
+            transform: scale(0.8);
+        }
+
+        20% {
+            opacity: 100%;
+            transform: scale(1)
+        }
+
+        40% {
+            opacity: 0%;
+            transform: scale(0.8);
+        }
+
+        60% {
+            opacity: 100%;
+            transform: scale(1)
+        }
+
+        80% {
+            opacity: 0%;
+            transform: scale(0.8);
+        }
+
+        100% {
+            opacity: 100%;
+            transform: scale(1)
+        }
+    }
+
+
+    &::after {
+        content: "";
+        animation: change 10s linear infinite;
+    }
+
+    @keyframes change {
+        20% {
+        content: "${props => props.text["0"]}";;
+        }
+        60% {
+            content: "${props => props.text[1]}";
+        }
+        100% {
+            content: "${props => props.text[2]}";
+        }
+    }
 `;
 
 export const VideoBg = styled.video `
@@ -28,6 +86,5 @@ export const VideoBg = styled.video `
     margin: 0;
     padding: 0;
     /* background-color: black; */
-    opacity: 0.2;
-    z-index: -1;
+    opacity: 0.4;
 `;
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,28 +3,20 @@ import Navbar from '../components/Navbar';
 import { AnimatedText, Wrapper, HeaderText, VideoBg, Wrapperd } from './PageStyles';
 import Classroom from "../videos/classroom.mp4"
 
-const AnimatedTextTitles = [
-    {
-        text: "Putting a twist to",
-    }, 
-
-    {
-        text: "Adding interactivity in",
-    },
-
-    {
-        text: "",
-    },
-]
+const AnimatedTextTitles = ["Putting a twist in", "Adding interactivity into", "Utilizing technology in"]
 
 function Home () {
     return (
         <div>
             <Navbar></Navbar>
-            <Wrapper index="1" height="85vh" width="100vw" bgcolor="#17E7AC">
-                <VideoBg index="-1" autoPlay loop muted src={Classroom} type="videos/mp4"></VideoBg>
-                <HeaderText index="3" color="#491985" pad="9rem">Putting a twist to</HeaderText>
-                <HeaderText index="3" color="black" pad="0.5rem">the classroom.</HeaderText>
+            <Wrapper index="1" pos="relative" height="85vh" width="100vw" bgcolor="#17E7AC">
+                <Wrapper index="-1" pos="absolute" height="85vh" width="100vw">
+                    <VideoBg autoPlay loop muted src={Classroom} type="videos/mp4"></VideoBg>
+                </Wrapper>
+                <Wrapper index="3" pos="relative" height="auto" width="auto">                    
+                    <AnimatedText color="#491985" pad="9rem" text={AnimatedTextTitles}></AnimatedText>
+                    <HeaderText color="black" pad="0.5rem">the classroom.</HeaderText>
+                </Wrapper>
             </Wrapper>
         </div>
     )
